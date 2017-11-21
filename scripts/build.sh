fb4c5c177da0176d2991fbe83828ec71b20a20e4
Olivier Louvignes <olivier@mgcrea.io>
2017-11-21 23:58:14 +0100

    feat(update): minor changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,11 +10,16 @@ envsubst < templates/influxdb.conf > "volumes/config/influxdb.conf"
 echo "Building \"kapacitor.conf\" via envsubst"
 envsubst < templates/kapacitor.conf > "volumes/config/kapacitor.conf"
 
+ENV_VARS=""
+if [[ -v INFLUXDB_DB ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_DB=${INFLUXDB_DB}"; fi
+if [[ -v INFLUXDB_ADMIN_USER ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_ADMIN_USER=${INFLUXDB_ADMIN_USER}"; fi
+if [[ -v INFLUXDB_ADMIN_PASSWORD ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_ADMIN_PASSWORD=${INFLUXDB_ADMIN_PASSWORD}"; fi
+if [[ -v INFLUXDB_USER ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_USER=${INFLUXDB_USER}"; fi
+if [[ -v INFLUXDB_USER_PASSWORD ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_USER_PASSWORD=${INFLUXDB_USER_PASSWORD}"; fi
+if [[ -v INFLUXDB_READ_USER ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_READ_USER=${INFLUXDB_READ_USER}"; fi
+if [[ -v INFLUXDB_READ_USER_PASSWORD ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_READ_USER_PASSWORD=${INFLUXDB_READ_USER_PASSWORD}"; fi
+if [[ -v INFLUXDB_WRITE_USER ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_WRITE_USER=${INFLUXDB_WRITE_USER}"; fi
+if [[ -v INFLUXDB_WRITE_USER_PASSWORD ]]; then ENV_VARS="${ENV_VARS} -e INFLUXDB_WRITE_USER_PASSWORD=${INFLUXDB_WRITE_USER_PASSWORD}"; fi
 
-# docker run --rm influxdb:1.4.2-alpine influxd config > volumes/config/influxdb.conf
-# docker run --rm \
-#       -e INFLUXDB_DB=db0 -e INFLUXDB_ADMIN_ENABLED=true
-#       -e INFLUXDB_ADMIN_USER=admin -e INFLUXDB_ADMIN_USER=supersecretpassword
-#       -e INFLUXDB_USER=telegraf -e INFLUXDB_USER_PASSWORD=secretpassword
-#       -v $PWD:/var/lib/influxdb \
-#       /init-influxdb.sh
+echo "Initializing InfluxDB database"
+docker run --rm $ENV_VARS -v $PWD/volumes/influxdb:/var/lib/influxdb influxdb:${INFLUXDB_VERSION} /init-influxdb.sh
