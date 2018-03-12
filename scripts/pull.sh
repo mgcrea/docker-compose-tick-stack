0a6b7e278376d78c4bb56842d8ae016e6b06dcfa
Olivier Louvignes <olivier@mgcrea.io>
2018-03-13 00:34:51 +0100

    feat(update): minor changes

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-source .env
+# source .env
 
-docker pull telegraf:${TELEGRAF_VERSION}
-docker pull influxdb:${INFLUXDB_VERSION}
-docker pull chronograf:${CHRONOGRAF_VERSION}
-docker pull kapacitor:${KAPACITOR_VERSION}
+# docker pull telegraf:${TELEGRAF_VERSION}
+# docker pull influxdb:${INFLUXDB_VERSION}
+# docker pull chronograf:${CHRONOGRAF_VERSION}
+# docker pull kapacitor:${KAPACITOR_VERSION}
+
+cat docker-compose.yml | grep image: | cut -d':' -f2-3 | while read line; do docker pull $line; done
